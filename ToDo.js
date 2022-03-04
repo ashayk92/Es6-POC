48e428cbea5f5a7d8d3f0f97bbbd267c704068a2
ashayk <ashayk@cybage.com>
2022-03-04 21:54:34 +0530

    Changes made for handling state persistence on page refresh.

--- ToDo.js
+++ ToDo.js
@@ -1,10 +1,15 @@
-// window.onload = () => {
-//   if (localStorage.getItem("taskList")) {
-//     const nodeList = JSON.parse(localStorage["taskList"]);
-//     let list = document.getElementById("list");
-//     [...nodeList].map((element) => list.appendChild(element));
-//   }
-// };
+window.onload = () => {
+  if (localStorage.getItem("taskList")) {
+    const nodeList = JSON.parse(localStorage["taskList"]);
+    const list = document.getElementById("list");
+    taskArray = [...nodeList];
+    const refreshedList = nodeList.map((element) => {
+      return getElement(element);
+    });
+    [...refreshedList].map((ele) => list.appendChild(ele));
+  }
+};
+let taskArray = [];
 
 const getAllNodes = () => {
   const list = document.getElementById("list");
@@ -13,19 +18,24 @@ const getAllNodes = () => {
 };
 const addTask = () => {
   const taskName = document.getElementById("taskId");
-
-  const ele = getElement(taskName.value);
+  const taskObj = { name: taskName.value, checked: false };
+  taskArray.push(taskObj);
+  const ele = getElement(taskObj);
   list.appendChild(ele);
-  // let childNodes = list.childNodes;
-  // localStorage["taskList"] = JSON.stringify([...childNodes]);
+
+  localStorage["taskList"] = JSON.stringify([...taskArray]);
   taskName.value = null;
 };
 
-const getElement = (taskName) => {
+const getElement = ({ name, checked }) => {
   let li = document.createElement("li");
-  li.innerText = taskName;
+  li.innerText = name;
   let checkbox = document.createElement("INPUT");
   checkbox.setAttribute("type", "checkbox");
+  checkbox.checked = checked;
+  checked
+    ? (li.style.textDecoration = "line-through")
+    : (li.style.textDecoration = "none");
   checkbox.onchange = (event) => completeTask(event, li);
   let deleteButton = document.createElement("INPUT");
   deleteButton.setAttribute("type", "button");
@@ -43,8 +53,10 @@ const deleteTask = (_event, ele) => {
     let list = document.getElementById("list");
     const nodeList = getAllNodes();
     nodeList.splice(index, 1);
+    taskArray.splice(index, 1);
     list.innerHTML = "";
     nodeList.map((element) => list.appendChild(element));
+    localStorage["taskList"] = JSON.stringify([...taskArray]);
   }
 };
 
@@ -54,10 +66,13 @@ const completeTask = (event, ele) => {
   if (checked === true) {
     ele.style.textDecoration = "line-through";
     getAllNodes[index] = ele;
+    taskArray[index].checked = true;
   } else {
     ele.style.textDecoration = "none";
     getAllNodes[index] = ele;
+    taskArray[index].checked = false;
   }
   let list = document.getElementById("list");
   getAllNodes().map((element) => list.appendChild(element));
+  localStorage["taskList"] = JSON.stringify([...taskArray]);
 };
